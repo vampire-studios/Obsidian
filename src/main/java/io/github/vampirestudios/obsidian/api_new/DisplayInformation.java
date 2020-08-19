a926826036d8ac34b0324778e3cd45ee4dada328
OliviaTheVampire <sindrefagerheim53@gmail.com>
2020-08-19 07:37:22 +0200

    removed the api_new package

--- src/main/java/io/github/vampirestudios/obsidian/api_new/DisplayInformation.java
+++
@@ -1,8 +0,0 @@
-package io.github.vampirestudios.obsidian.api_new;
-
-public class DisplayInformation {
-
-    public TextureAndModelInformation model;
-    public TooltipInformation[] lore = new TooltipInformation[0];
-
-}
--- src/main/java/io/github/vampirestudios/obsidian/api_new/block/OreRangeConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.vampirestudios.obsidian.api_new.block;
-
-public class OreRangeConfig {
-
-    public int bottom_offset;
-    public int top_offset;
-    public int maximum;
-
-}
