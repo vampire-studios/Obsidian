9718b83c34015627c3f3b7b595df3c976b2b2c33
OliviaTheVampire <sindrefagerheim53@gmail.com>
2021-10-05 07:34:09 +0200

    started adding a dependency system

--- src/main/java/io/github/vampirestudios/obsidian/configPack/ObsidianAddonInfo.java
+++ src/main/java/io/github/vampirestudios/obsidian/configPack/ObsidianAddonInfo.java
@@ -1,17 +1,26 @@
 package io.github.vampirestudios.obsidian.configPack;
 
 import java.util.List;
+import java.util.Locale;
 
 public class ObsidianAddonInfo {
 
     public String displayName;
-    public String folderName;
-    public String namespace;
+    public String folderName = "";
+    public String namespace = folderName.toLowerCase(Locale.ROOT).replace(" ", "_");
     public String version;
     public String description;
+    public String license = "MIT";
     public List<String> authors;
+    public List<Dependency> dependencies;
     public int addonVersion;
     public boolean hasAssets = false;
     public boolean hasData = false;
+
+    public static class Dependency {
+        public String namespace;
+        public String version;
+        public String type;
+    }
 
 }
