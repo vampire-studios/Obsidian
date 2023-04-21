a334f417508e98ef98407914283bacc8b7b29c4a
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-04-21 09:11:25 +0200

    idk

---
+++ src/main/java/io/github/vampirestudios/obsidian/api/obsidian/entity/components/behaviour/RandomSearchAndDigBehaviourComponent.java
@@ -0,0 +1,13 @@
+package io.github.vampirestudios.obsidian.api.obsidian.entity.components.behaviour;
+
+import io.github.vampirestudios.obsidian.api.obsidian.entity.Component;
+
+import java.util.List;
+
+public class RandomSearchAndDigBehaviourComponent extends Component {
+
+    public List<String> items;
+    public float look_distance = 8.0F;
+    public int[] look_time = new int[] {2, 4};
+
+}
